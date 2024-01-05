74e5d5c9e2a96c5cbb186939039c9c49a96ad068
Paula <146264809+paulagvp@users.noreply.github.com>
2024-01-05 13:22:04 -0300

    Update Estacionamento.cs

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -7,6 +7,59 @@ namespace Desafio_de_Porjeto___Estacionamneto_no_C_.Models
 {
     public class Estacionamento
     {
-        
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine();
+            veiculos.Add(placa);
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine();
+            // string placa = "";
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                int horas = 0;
+                decimal valorTotal = 0; 
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                horas = Int32.Parse(Console.ReadLine());
+                valorTotal = precoInicial + precoPorHora * horas;
+
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                foreach (string item in veiculos)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
     }
 }
